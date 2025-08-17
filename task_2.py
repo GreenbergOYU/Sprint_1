490e52f26350730c6821822899dd74c5ffb57399
Olga Grinberg <grinberg85@yandex.ru>
2025-08-17 11:52:03 +0300

    Правки по замечанию

--- task_2.py
+++ task_2.py
@@ -1,7 +1,6 @@
 class Tester:
-    def __init__(self, name,deadline=True):
+    def __init__(self, name):
         self.name = name
-        self.deadline = deadline
     def work_hard(self, deadline=True):
         if deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
@@ -10,4 +9,4 @@ class Tester:
 tester_1 = Tester(name='tester_1')
 tester_1.work_hard(deadline=False)  
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   
+tester_2.work_hard(deadline=True)
